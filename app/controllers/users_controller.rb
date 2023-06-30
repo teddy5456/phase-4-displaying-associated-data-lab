bbedbffda482f15d7ae331dc38cc4c2b6bba362c
teddy <teddywambua5456@gmail.com>
2023-06-30 09:43:11 +0300

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
-end
+    def show
+      user = User.find(params[:id])
+      render json: user.as_json(include: :items)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  get '/users/:id', to: 'users#show'
+  get '/items', to: 'items#index'
 end
